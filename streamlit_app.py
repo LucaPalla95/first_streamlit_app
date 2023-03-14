6a41a844840570865673da6267d1c2f8b0f85d5b
Luca Palla <100412354+LucaPalla95@users.noreply.github.com>
2023-03-14 12:00:32 +0100

    Update streamlit_app.py
    adding try and except structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,17 @@ streamlit.dataframe(fruits_to_show)
 
 #importing data from fruityvice
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# Normalization of json data
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# make it in a dataframe
-streamlit.dataframe(fruityvice_normalized)
-
-#Insert entry box and api call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+  streamlit.error()
 
 # stop in order to work and fix problem
 streamlit.stop()
