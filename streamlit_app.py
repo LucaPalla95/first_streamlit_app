a9ff5fc51175552e58ea98b4bb6383513eec2192
Luca Palla <100412354+LucaPalla95@users.noreply.github.com>
2023-03-14 11:50:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urlib.error import urlerror
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
