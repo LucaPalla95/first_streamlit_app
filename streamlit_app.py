7d9a2b7d428fbec4e108b5c972f2d34e16350295
Luca Palla <100412354+LucaPalla95@users.noreply.github.com>
2023-03-14 11:28:36 +0100

    Update streamlit_app.py
    connection streamlit-snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,11 @@ streamlit.dataframe(fruityvice_normalized)
 #Insert entry box and api call
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+# snowflake-streamlit connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
